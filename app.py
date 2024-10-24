c1381bf9fe8b3f08097350b083e43aea1492e7bd
Morgrime <daniel2000@mail.ru>
2024-10-24 16:58:25 +0500

    Test line charts

--- app.py
+++ app.py
@@ -1,36 +1,13 @@
 import streamlit as st
-from datetime import time, datetime
+import pandas as pd
+import numpy as np
+
+st.header('Line chart')
+
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
 
 
-"""
-sliders
-"""
-# st.header('st.slider')
-
-# st.subheader('Slider')
-# age = st.slider('How old are you?', 0, 130, 25)
-# st.write(f"I'm {age} years old")
-
-
-# st.subheader('Range slider')
-# values = st.slider('Select a range of values', 0.0, 100.0, (25.0, 75.0))
-# st.write('Values:', values)
-
-# st.subheader('Range time slider')
-# appointment = st.slider(
-#     'Schedule your appointment',
-#     value=(time(11, 30), time(12, 45)))
-# st.write('You\'re scheduled for:', appointment)
-
-# st.subheader('Datetime slider')
-# start_time = st.slider(
-#     'When do you start?',
-#     value=datetime(2020, 1, 1, 9, 30),
-#     format="MM/DD/YY - hh:mm")
-# st.write("Start time:", start_time)
-
-# st.subheader('Select slider')
-# selectSlider = st.select_slider(
-#     label='Test slider',
-#     options=['first', 'secodn', 'third', 'fourth'])
-# st.write('Your chosen option is:', selectSlider)
