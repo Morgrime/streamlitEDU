0a187fbbbd7c77a9c9870ee717058713dc4f52a2
Morgrime <daniel2000@mail.ru>
2024-10-25 10:32:41 +0500

    Initial commit

---
+++ sliders.py
@@ -0,0 +1,32 @@
+"""
+sliders
+"""
+# st.header('st.slider')
+
+# st.subheader('Slider')
+# age = st.slider('How old are you?', 0, 130, 25)
+# st.write(f"I'm {age} years old")
+
+
+# st.subheader('Range slider')
+# values = st.slider('Select a range of values', 0.0, 100.0, (25.0, 75.0))
+# st.write('Values:', values)
+
+# st.subheader('Range time slider')
+# appointment = st.slider(
+#     'Schedule your appointment',
+#     value=(time(11, 30), time(12, 45)))
+# st.write('You\'re scheduled for:', appointment)
+
+# st.subheader('Datetime slider')
+# start_time = st.slider(
+#     'When do you start?',
+#     value=datetime(2020, 1, 1, 9, 30),
+#     format="MM/DD/YY - hh:mm")
+# st.write("Start time:", start_time)
+
+# st.subheader('Select slider')
+# selectSlider = st.select_slider(
+#     label='Test slider',
+#     options=['first', 'secodn', 'third', 'fourth'])
+# st.write('Your chosen option is:', selectSlider)
